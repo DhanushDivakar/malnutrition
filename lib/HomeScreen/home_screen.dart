fd7c3211599511535d413603316e2a414d363c46
Dhanush D <dhanushsujatha123@gmail.com>
2023-04-16 22:28:43 +0530

    change

--- lib/HomeScreen/home_screen.dart
+++ lib/HomeScreen/home_screen.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'dart:math' as math;
 
-import '../services/getlocation.dart';
+import 'package:malnutrition/services/getlocation.dart';
+import 'package:malnutrition/services/update_loc.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key}) : super(key: key);
@@ -16,51 +17,241 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('My App'),
+      appBar: AppBar(
+        title: const Text('My App'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              child: Text('Menu'),
+              decoration: BoxDecoration(),
+            ),
+            ListTile(
+              title: const Text('Malnutrition Checker'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return const MalnutritionChecker();
+                }));
+              },
+            ),
+            const Divider(),
+            ListTile(
+              title: const Text('Track'),
+              onTap: () {
+                // Do something when Option 2 is tapped
+              },
+            ),
+          ],
         ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              DrawerHeader(
-                child: Text('Menu'),
-                decoration: BoxDecoration(),
-              ),
-              ListTile(
-                title: const Text('Malnutrition Checker'),
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return const MalnutritionChecker();
-                  }));
-                },
-              ),
-              const Divider(),
-              ListTile(
-                title: const Text('Track'),
-                onTap: () {
-                  // Do something when Option 2 is tapped
-                },
-              ),
-            ],
-          ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return const AddChild();
+                }));
+              },
+              child: Text("Add child"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return const UpdateLocation();
+                }));
+              },
+              child: const Text("Update Location"),
+            ),
+          ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              TextButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return const AddChild();
-                    }));
-                  },
-                  child: Text("Add child"))
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
+
+// class AddChild extends StatefulWidget {
+//   const AddChild({super.key});
+
+//   @override
+//   State<AddChild> createState() => _AddChildState();
+// }
+
+// class _AddChildState extends State<AddChild> {
+//   final _formKey = GlobalKey<FormState>();
+//   String? _childName, _aadharNumber, _guardianName;
+//   Position? _position;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     getLocation();
+//   }
+
+//   Future<void> getLocation() async {
+//     // Test if location services are enabled.
+//     Geolocator.requestPermission();
+//     bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+//     if (!serviceEnabled) {
+//       // Location services are not enabled don't continue
+//       // accessing the position and request users of the
+//       // App to enable the location services.
+//       return Future.error('Location services are disabled.');
+//     }
+
+//     LocationPermission permission = await Geolocator.checkPermission();
+//     if (permission == LocationPermission.denied) {
+//       permission = await Geolocator.requestPermission();
+//       if (permission == LocationPermission.deniedForever) {
+//         // Permissions are denied forever, handle appropriately.
+//         return Future.error(
+//             Exception('Location permissions are permanently denied.'));
+//       }
+
+//       if (permission == LocationPermission.denied) {
+//         // Permissions are denied, next time you could try
+//         // requesting permissions again (this is also where
+//         // Android's shouldShowRequestPermissionRationale
+//         // returned true. According to Android guidelines
+//         // your App should show an explanatory UI now.
+//         return Future.error(Exception('Location permissions are denied.'));
+//       }
+//     }
+
+//     // When we reach here, permissions are granted and we can
+//     // continue accessing the position of the device.
+
+//     Position position = await Geolocator.getCurrentPosition(
+//         desiredAccuracy: LocationAccuracy.high);
+//     setState(() {
+//       _position = position;
+//     });
+//   }
+
+//   // void _getCurrentLocation() async {
+//   //   bool serviceEnabled;
+//   //   LocationPermission permission;
+//   //   Geolocator.requestPermission();
+
+//   //   serviceEnabled = await Geolocator.isLocationServiceEnabled();
+//   //   if (!serviceEnabled) {
+//   //     return Future.error('Location services are disabled.');
+//   //   }
+
+//   //   permission = await Geolocator.checkPermission();
+//   //   if (permission == LocationPermission.denied) {
+//   //     permission = await Geolocator.requestPermission();
+//   //     if (permission == LocationPermission.denied) {
+//   //       return Future.error('Location permissions are denied');
+//   //     }
+//   //   }
+
+//   //   if (permission == LocationPermission.deniedForever) {
+//   //     return Future.error(
+//   //         'Location permissions are permanently denied, we cannot request permissions.');
+//   //   }
+
+//   //   Position position = await Geolocator.getCurrentPosition();
+//   //   setState(() {
+//   //     _position = position;
+//   //   });
+//   // }
+
+//   void _submitForm() async {
+//     if (_formKey.currentState!.validate()) {
+//       _formKey.currentState!.save();
+//       try {
+//         // await FirebaseFirestore.instance.collection('children_details').add({
+//         //   'childName': _childName,
+//         //   'aadharNumber': _aadharNumber,
+//         //   'guardianName': _guardianName,
+//         // });
+//         print(_childName);
+//         print(_aadharNumber);
+//         print(_guardianName);
+//         ScaffoldMessenger.of(context).showSnackBar(
+//           SnackBar(content: Text('Child added successfully')),
+//         );
+//       } catch (error) {
+//         ScaffoldMessenger.of(context).showSnackBar(
+//           SnackBar(content: Text('Failed to add child')),
+//         );
+//       }
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Add Child'),
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.all(16),
+//         child: Form(
+//           key: _formKey,
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.stretch,
+//             children: [
+//               TextFormField(
+//                 decoration:
+//                     InputDecoration(labelText: 'Child Name as in Aadhar Card'),
+//                 validator: (value) {
+//                   if (value!.isEmpty) {
+//                     return 'Please enter a name';
+//                   }
+//                   return null;
+//                 },
+//                 onSaved: (value) {
+//                   _childName = value!;
+//                 },
+//               ),
+//               TextFormField(
+//                 decoration: InputDecoration(labelText: 'Aadhar Number'),
+//                 validator: (value) {
+//                   if (value!.isEmpty) {
+//                     return 'Please enter a number';
+//                   }
+//                   if (!RegExp(r'^[2-9]{1}[0-9]{3}[0-9]{4}[0-9]{4}$')
+//                       .hasMatch(value)) {
+//                     return 'Please enter a valid 12-digit Aadhar number';
+//                   }
+//                   return null;
+//                 },
+//                 onSaved: (value) {
+//                   _aadharNumber = value!;
+//                 },
+//               ),
+//               TextFormField(
+//                 decoration: InputDecoration(labelText: 'Guardian Name'),
+//                 validator: (value) {
+//                   if (value!.isEmpty) {
+//                     return 'Please enter a name';
+//                   }
+//                   return null;
+//                 },
+//                 onSaved: (value) {
+//                   _guardianName = value!;
+//                 },
+//               ),
+//               SizedBox(height: 16),
+//               if (_position != null)
+//                 Text(
+//                     'Latitude: ${_position?.latitude}, Longitude: ${_position?.longitude}'),
+//               SizedBox(height: 16),
+//               ElevatedButton(
+//                 onPressed: _submitForm,
+//                 child: Text('Add Child'),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 class MalnutritionChecker extends StatefulWidget {
   const MalnutritionChecker({super.key});
---
+++ lib/services/update_loc.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class UpdateLocation extends StatefulWidget {
+  const UpdateLocation({super.key});
+
+  @override
+  State<UpdateLocation> createState() => _UpdateLocationState();
+}
+
+class _UpdateLocationState extends State<UpdateLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(),
+    );
+  }
+}
