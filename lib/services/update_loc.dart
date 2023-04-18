c4b6dad6bb12f0bbeb85103cd179fcded019a6b2
Dhanush D <dhanushsujatha123@gmail.com>
2023-04-19 00:38:14 +0530

    latest

--- lib/services/update_loc.dart
+++ lib/services/update_loc.dart
@@ -73,11 +73,72 @@ class _UpdateLocationState extends State<UpdateLocation> {
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
                 labelText: 'Aadhaar Number',
+                hintText: 'Aadhaar Number',
+                border: OutlineInputBorder(),
               ),
             ),
             SizedBox(height: 16.0),
             ElevatedButton(
-              onPressed: _searchAadhar,
+              onPressed: () async {
+                FocusScope.of(context).unfocus();
+                final String aadharNumberCo =
+                    _aadharNumberController.text.trim();
+
+                final CollectionReference collectionReference =
+                    FirebaseFirestore.instance.collection('children_details');
+                Query query = collectionReference
+                    .where('aadharNumber', isEqualTo: aadharNumberCo)
+                    .limit(1);
+                QuerySnapshot querySnapshot = await query.get();
+                final docData =
+                    querySnapshot.docs.map((doc) => doc.data()).toList();
+
+                if (docData.isNotEmpty) {
+                  print("found");
+                  setState(() {
+                    aadharNumber = aadharNumberCo;
+                    ScaffoldMessenger.of(context)
+                        .showSnackBar(const SnackBar(content: Text('Found')));
+                  });
+                } else {
+                  print("not found");
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Aadhar number not found')));
+                  setState(() {
+                    aadharNumber = null;
+                  });
+                }
+                // ignore: use_build_context_synchronously
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        title: Text('Result'),
+                        content: Text('Result '),
+                        actions: [
+                          ElevatedButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text('Go Back'))
+                        ],
+                      );
+                    });
+
+                // await FirebaseFirestore.instance
+                //     .collection('children_details')
+                //     .where('aadharNumber', isEqualTo: aadharNumberCo)
+                //     .get()
+                //     .then((event) {
+                //   if (event.docs.isNotEmpty) {
+                //     Map<String, dynamic> documentData =
+                //         event.docs.single.data()[1]['aadharNumber'];
+                //     print(documentData); //if it is a single document
+                //   }
+                // }).catchError((e) => print("error fetching data: $e"));
+              },
               child: Text('Search'),
             ),
             SizedBox(height: 16.0),
