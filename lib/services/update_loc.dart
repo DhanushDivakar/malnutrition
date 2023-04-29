3a89dcfe0513425e8e409021d04bd3b880dfedc2
Dhanush D <dhanushsujatha123@gmail.com>
2023-04-29 21:40:47 +0530

    ch

--- lib/services/update_loc.dart
+++ lib/services/update_loc.dart
@@ -71,15 +71,7 @@ class _UpdateLocationState extends State<UpdateLocation> {
               maxLength: 12,
               controller: _aadharNumberController,
               keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: 'Aadhaar Number',
-                hintText: 'Aadhaar Number',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () async {
+              onSubmitted: (String value) async {
                 FocusScope.of(context).unfocus();
                 final String aadharNumberCo =
                     _aadharNumberController.text.trim();
@@ -97,13 +89,10 @@ class _UpdateLocationState extends State<UpdateLocation> {
                   print("found");
                   setState(() {
                     aadharNumber = aadharNumberCo;
-                    ScaffoldMessenger.of(context)
-                        .showSnackBar(const SnackBar(content: Text('Found')));
                   });
                 } else {
                   print("not found");
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Aadhar number not found')));
+
                   setState(() {
                     aadharNumber = null;
                   });
@@ -116,7 +105,7 @@ class _UpdateLocationState extends State<UpdateLocation> {
                         return AlertDialog(
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10)),
-                          title: Text('Search'),
+                          title: const Text('Search'),
                           content: Text(
                               'Aadhaar number ${_aadharNumberController.text} found'),
                           actions: [
@@ -136,24 +125,103 @@ class _UpdateLocationState extends State<UpdateLocation> {
                         return AlertDialog(
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10)),
-                          title: Text('Search'),
+                          title: const Text('Search'),
                           content: Text(
-                              'Aadhaar number ${_aadharNumberController.text} not found'),
+                              'Aadhaar number ${_aadharNumberController.text} not found, please register'),
                           actions: [
                             ElevatedButton(
                               onPressed: () {
                                 Navigator.pop(context);
                               },
-                              child: Text('Go Back'),
+                              child: const Text('Go Back'),
                             )
                           ],
                         );
                       });
                 }
+
+                // Do something with the submitted value
               },
-              child: Text('Search'),
-              // child: Text('Search'),
+              decoration: InputDecoration(
+                labelText: 'Aadhaar Number',
+                hintText: 'Aadhaar Number',
+                border: OutlineInputBorder(),
+              ),
             ),
+            const SizedBox(height: 16.0),
+            // ElevatedButton(
+            //   onPressed: () async {
+            //     FocusScope.of(context).unfocus();
+            //     final String aadharNumberCo =
+            //         _aadharNumberController.text.trim();
+
+            //     final CollectionReference collectionReference =
+            //         FirebaseFirestore.instance.collection('children_details');
+            //     Query query = collectionReference
+            //         .where('aadharNumber', isEqualTo: aadharNumberCo)
+            //         .limit(1);
+            //     QuerySnapshot querySnapshot = await query.get();
+            //     final docData =
+            //         querySnapshot.docs.map((doc) => doc.data()).toList();
+
+            //     if (docData.isNotEmpty) {
+            //       print("found");
+            //       setState(() {
+            //         aadharNumber = aadharNumberCo;
+            //       });
+            //     } else {
+            //       print("not found");
+
+            //       setState(() {
+            //         aadharNumber = null;
+            //       });
+            //     }
+            //     if (docData.isNotEmpty) {
+            //       // ignore: use_build_context_synchronously
+            //       showDialog(
+            //           context: context,
+            //           builder: (BuildContext context) {
+            //             return AlertDialog(
+            //               shape: RoundedRectangleBorder(
+            //                   borderRadius: BorderRadius.circular(10)),
+            //               title: const Text('Search'),
+            //               content: Text(
+            //                   'Aadhaar number ${_aadharNumberController.text} found'),
+            //               actions: [
+            //                 ElevatedButton(
+            //                     onPressed: () {
+            //                       Navigator.pop(context);
+            //                     },
+            //                     child: Text('yes'))
+            //               ],
+            //             );
+            //           });
+            //     } else {
+            //       // ignore: use_build_context_synchronously
+            //       showDialog(
+            //           context: context,
+            //           builder: (BuildContext context) {
+            //             return AlertDialog(
+            //               shape: RoundedRectangleBorder(
+            //                   borderRadius: BorderRadius.circular(10)),
+            //               title: const Text('Search'),
+            //               content: Text(
+            //                   'Aadhaar number ${_aadharNumberController.text} not found, please register'),
+            //               actions: [
+            //                 ElevatedButton(
+            //                   onPressed: () {
+            //                     Navigator.pop(context);
+            //                   },
+            //                   child: const Text('Go Back'),
+            //                 )
+            //               ],
+            //             );
+            //           });
+            //     }
+            //   },
+            //   child: const Text('Search'),
+            //   // child: Text('Search'),
+            // ),
             const SizedBox(height: 16.0),
 
             // StreamBuilder(
